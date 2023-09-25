d468d9ac69f2587e89e119350a94877b4c49473f
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-25 17:46:58 +0800

    add loading animation and route

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/card";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Loader2 } from "lucide-react";
 
 //TODO: CHANGE API URL
 interface Coin {
@@ -25,6 +27,7 @@ interface Coin {
 
 export default function Showcase() {
   const [coins, setCoins] = useState<Coin[]>([]);
+  const [loading, setLoading] = useState(true);
   const URL = "https://jsonplaceholder.org/users";
   useEffect(() => {
     //GET
@@ -32,29 +35,45 @@ export default function Showcase() {
       .get<Coin[]>(URL)
       .then((response) => {
         setCoins(response.data.slice(0, 4));
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(true);
       });
   }, []);
   return (
-    <div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
-      {coins.map((coin) => (
-        <Card key={coin.id} className="bg-zinc-900 border border-zinc-700">
-          <CardHeader>
-            <CardTitle className="text-white">{coin.firstname}</CardTitle>
-            <CardDescription>{coin.firstname}</CardDescription>
-          </CardHeader>
-          <CardContent className="text-white">
-            <p>
-              {coin.address.city} {coin.address.street} {coin.address.suite}
-            </p>
-          </CardContent>
-          <CardFooter className="text-muted-foreground">
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      ))}
+    <div>
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <Loader2 className="animate-spin w-20 h-20" />
+        </div>
+      ) : (
+        <div className="container px-0 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+          {coins.map((coin) => (
+            <Link to={`/coin/${coin.id}`} key={coin.id}>
+              <Card
+                key={coin.id}
+                className="bg-zinc-900 border border-zinc-700"
+              >
+                <CardHeader>
+                  <CardTitle className="text-white">{coin.firstname}</CardTitle>
+                  <CardDescription>{coin.firstname}</CardDescription>
+                </CardHeader>
+                <CardContent className="text-white">
+                  <p>
+                    {coin.address.city} {coin.address.street}{" "}
+                    {coin.address.suite}
+                  </p>
+                </CardContent>
+                <CardFooter className="text-muted-foreground">
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
