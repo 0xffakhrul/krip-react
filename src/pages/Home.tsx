0f2ba715ab29c4d2554377616ff5af862d198c85
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-25 17:48:25 +0800

    migrate homepage to indiv page instead of index

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+// import Navbar from "@/components/Navbar";
+import grid from "../assets/grid.svg";
+import Headline from "@/components/Headline";
+import Showcase from "@/components/Showcase";
+import CoinTable from "@/components/CoinList/CoinTable";
+
+export default function Home() {
+  return (
+    <div>
+      <main
+        className="mx-auto max-w-7xl px-6 min-h-screen bg-[length:1200px_800px] bg-top bg-no-repeat"
+        style={{ backgroundImage: `url(${grid})` }}
+      >
+        <Headline />
+        <Showcase />
+        <CoinTable />
+      </main>
+    </div>
+  );
+}
