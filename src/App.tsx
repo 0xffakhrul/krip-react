3a26460cebda846648faaff2d38bd9a34d863f81
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-25 17:46:27 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import Navbar from "./components/Navbar";
-import grid from "./assets/grid.svg";
-import Headline from "./components/Headline";
-import Showcase from "./components/Showcase";
+import Coin from "./pages/Coin";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="relative flex flex-col pb-40 text-zinc-200 bg-zinc-950 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] bg-[length:1200px_800px] from-zinc-400/10 to-60% to-transparent bg-top bg-no-repeat selection:bg-zinc-500/20 antialiased">
       <Navbar />
-      <main
-        className="mx-auto min-h-screen bg-[length:1200px_800px] bg-top bg-no-repeat"
-        style={{ backgroundImage: `url(${grid})` }}
-      >
-        <Headline />
-        <Showcase />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
