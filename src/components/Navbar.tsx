8cc0c12474f2d374ddceb4f37a00f284513cf6b5
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-25 17:46:44 +0800

    ui change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="w-full py-2 border-b backdrop-blur-xl backdrop-saturate-150 bg-zinc-900/40 border-zinc-700">
+    <nav className="w-full px-6 py-2 border-b backdrop-blur-xl backdrop-saturate-150 bg-zinc-900/40 border-zinc-700">
       <div className="max-w-7xl mx-auto h-12 flex items-center justify-between">
         <p className="font-bold text-xl">YOOOO</p>
         <ul className="flex gap-5 font-semibold text-lg">
