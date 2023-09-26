2acedfb489ec0fa78e4fd8dc71b2d2d4a0ca3530
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-26 18:24:34 +0800

    table adjustment

--- src/components/CoinList/CoinTable.tsx
+++ src/components/CoinList/CoinTable.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Coin, columns } from "./columns";
 import { DataTable } from "./data-table";
 import axios from "axios";
+import { motion } from "framer-motion";
+import { fromBottomFast } from "@/lib/animation";
 
 function CoinTable() {
   const [data, setData] = useState<Coin[]>([]);
@@ -10,7 +12,7 @@ function CoinTable() {
     async function fetchData() {
       try {
         const res = await axios.get<Coin[]>(
-          "https://jsonplaceholder.org/users"
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false"
         );
         const slicedData = res.data.slice(0, 20);
         setData(slicedData);
@@ -24,10 +26,21 @@ function CoinTable() {
 
   return (
     <div className="container px-0">
-      <h1 className="text-white font-bold text-4xl text-center py-12">
+      <motion.h1
+        className="text-white font-bold text-4xl text-center py-12"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 0.5 }}
+      >
         Market Update
-      </h1>
-      <DataTable columns={columns} data={data} />
+      </motion.h1>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 0.5 }}
+      >
+        <DataTable columns={columns} data={data} />
+      </motion.div>
     </div>
   );
 }
