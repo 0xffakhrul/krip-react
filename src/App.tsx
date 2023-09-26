18a52500f84aa4be55b05cfc8e7b3d1f59c7883a
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-09-26 18:23:55 +0800

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="relative flex flex-col pb-40 text-zinc-200 bg-zinc-950 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] bg-[length:1200px_800px] from-zinc-400/10 to-60% to-transparent bg-top bg-no-repeat selection:bg-zinc-500/20 antialiased">
+    <div className="min-h-screen text-zinc-200 bg-zinc-950 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] bg-[length:1200px_800px] from-zinc-400/10 to-60% to-transparent bg-top bg-no-repeat selection:bg-zinc-500/20 antialiased">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
